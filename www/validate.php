879973cd3304a1f0f6717ae33e57bb67c4e6014d
Yebeltal Asseged <37700392+yebeman@users.noreply.github.com>
2018-03-29 12:38:50 -0500

    Update validate.php

--- www/validate.php
+++ www/validate.php
@@ -6,8 +6,8 @@ $tableNo=$_POST['tableNo'];
 
 
  
-$emailAddress='d3dd44d9d83a6ae8c0c249ce90510757';//$_COOKIE["em"];
-$password='a195bc1c245976ac857b05f641d7ac88';//$_COOKIE["sub"];
+$emailAddress='*****';//$_COOKIE["em"];
+$password='*****';//$_COOKIE["sub"];
          
 $sql = mysql_query("SELECT * FROM `members` WHERE  emailAddressE='$emailAddress' AND password='$password'"); 
 $row = mysql_fetch_array($sql);
