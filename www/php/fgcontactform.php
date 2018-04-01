9aed58d04b328cc68c94902004f41862c1e1bc62
Yebeltal Asseged <37700392+yebeman@users.noreply.github.com>
2018-04-01 09:01:31 -0500

    Update fgcontactform.php

--- www/php/fgcontactform.php
+++ www/php/fgcontactform.php
@@ -1,17 +1,5 @@
 <?PHP
-/*
-    Contact Form from HTML Form Guide
 
-    This program is free software published under the
-    terms of the GNU Lesser General Public License.
-
-This program is distributed in the hope that it will
-be useful - WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE.
-
-@copyright html-form-guide.com 2010
-*/
 require_once("class.phpmailer.php");
 
 /*
